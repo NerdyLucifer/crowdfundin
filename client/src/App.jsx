ef2567ddf0e64918bad9a36a12ee3219a4693d66
NerdyLucifer <ashwat.kumarsingh.met20@itbhu.ac.in>
2022-06-20 09:20:34 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navigationbar/>
+      <h3 style={{color: 'red'}}>Connect the wallet to GOERLI TEST NETWORK</h3>
       <UploadInitiative/>
       <YourInitiatives/>
       <Initiatives/>
